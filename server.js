a8acd7a4b574c7e8000b8a8b1c90c40a1a1b2491
AdrianMSS <sesamaua@gmail.com>
2017-07-31 19:13:38 -0600

    Firebase Test 5

--- server.js
+++ server.js
@@ -27,28 +27,7 @@ admin.initializeApp({
   databaseURL: "https://soschat-6d3df.firebaseio.com"
 });
 
-// The topic name can be optionally prefixed with "/topics/".
-var topic = "alert";
 
-// See the "Defining the message payload" section below for details
-// on how to define a message payload.
-var payload = {
-  notification: {
-    title: "SOS App",
-    body: "CNE alert"
-  }
-};
-
-// Send a message to devices subscribed to the provided topic.
-admin.messaging().sendToTopic(topic, payload)
-  .then(function(response) {
-    // See the MessagingTopicResponse reference documentation for the
-    // contents of response.
-    console.log("Successfully sent message:", response);
-  })
-  .catch(function(error) {
-    console.log("Error sending message:", error);
-  });
 
 var app = express();
 app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
@@ -239,6 +218,29 @@ io.on('connection', function (socket) {
 		console.log(msg);
 		lastAlert=msg.type;
 		basicCall(lastAlert);
+		// The topic name can be optionally prefixed with "/topics/".
+		var topic = msg.city;
+		var body = "Alerta de tipo "+msg.type;
+
+		// See the "Defining the message payload" section below for details
+		// on how to define a message payload.
+		var payload = {
+		  notification: {
+		    title: "SOS App",
+		    body: body
+		  }
+		};
+
+		// Send a message to devices subscribed to the provided topic.
+		admin.messaging().sendToTopic(topic, payload)
+		  .then(function(response) {
+		    // See the MessagingTopicResponse reference documentation for the
+		    // contents of response.
+		    console.log("Successfully sent message:", response);
+		  })
+		  .catch(function(error) {
+		    console.log("Error sending message:", error);
+		  });
 		var message = {msg:msg, lat:msg.lat, long:msg.long, type:msg.type, position:msg.position, user:msg.user, city:msg.city};
 		azureServices.insertLocation(message, io);
 	})
