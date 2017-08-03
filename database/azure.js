4d9d696d24fadae88ae52c14cc47e8b79c361fbd
AdrianMSS <sesamaua@gmail.com>
2017-08-02 20:13:54 -0600

    Chats 2

--- database/azure.js
+++ database/azure.js
@@ -393,7 +393,7 @@ exports.userChats = function(req,res){
           var hasImg = false;
           if(!element.first)newChat=element.msg.substr(0,15);
           if(element.img) hasImg=true;
-          msg[index] = {PartitionKey:element.PartitionKey, type:element.type, msg:newChat, status:element.status, city:element.city, lat:element.lat,long:element.long,path:element.path, img:hasImg, msg:element.msg, count:element.count}
+          msg[index] = {PartitionKey:element.PartitionKey, RowKey:element.RowKey, type:element.type, msg:newChat, status:element.status, city:element.city, lat:element.lat,long:element.long,path:element.path, img:hasImg, msg:element.msg, count:element.count}
         });
         console.log(msg);
         res.send(200,{"chats":msg});
